35007e3ec7a053cbf799aa96885e5b42c953a572
Michael <mi.nicholson@duocuc.cl>
2026-04-01 19:28:38 -0300

    Testeo 1

---
+++ src/main/java/com/example/GestionRutinas/GestionRutinasApplication.java
@@ -0,0 +1,13 @@
+package com.example.GestionRutinas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionRutinasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionRutinasApplication.class, args);
+	}
+ //asd
+}
---
+++ src/test/java/com/example/GestionRutinas/GestionRutinasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GestionRutinas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionRutinasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
