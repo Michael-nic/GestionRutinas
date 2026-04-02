45952beb0125a58d26d77f06c0893ac068923bdb
Michael <mi.nicholson@duocuc.cl>
2026-04-01 21:26:58 -0300

    Final

--- src/main/java/com/example/GestionRutinas/Controller/RutinaControlador.java
+++ src/main/java/com/example/GestionRutinas/Controller/RutinaControlador.java
@@ -1,10 +1,13 @@
 package com.example.GestionRutinas.Controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.example.GestionRutinas.Model.Rutinas;
 import com.example.GestionRutinas.Services.RutinaService;
@@ -17,12 +20,16 @@ public class RutinaControlador {
     private RutinaService rutinaService;
 
     @GetMapping
-    public List<Rutinas> listarRutinas;
+    public List<Rutinas> listarRutinas(){
+        return rutinaService.getRutinas();
+    }
 
     @PostMapping
-    public Rutinas agregarRutina(@requestBody Rutinas _rutina){
+    public Rutinas agregarRutina(@RequestBody Rutinas _rutina){
         return rutinaService.saveRutinas(_rutina);
     }
+
+    
 
 
 
